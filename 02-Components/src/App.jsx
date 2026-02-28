a786ba55a581db3341c6ce4001b5e18d9bff52f5
Tarun Nagar <tarungolu321@gmail.com>
2026-02-28 19:44:06 +0530

    Added React components and initial setup

---
+++ 02-Components/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+import Button from './components/Button'
+
+const App = () => {
+  return (
+    <div>
+     <Navbar/>
+     <Navbar/>
+    <Card/>
+    <Card/>
+    <Button/>
+   </div>
+  )
+}
+
+export default App
---
+++ 02-Components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+    <div className='card'>
+        <h1>Tarun Nagar</h1>
+        <p>And im going to be the best software developer</p>
+    </div>
+    </>
+  )
+}
+
+export default Card
